4da7ce7abbdc37aa44b850494f9145dd004d20a1
SimonLindegaardM <slm311096@gmail.com>
2023-12-16 14:16:41 +0100

    added styling for login and navbar

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -2,6 +2,7 @@ import { Card, CardBody, HStack, Heading, Image, Text } from '@chakra-ui/react'
 import { Product } from '../hooks/useProducts'
 import StockIndicator from './StockIndicator'
 import { formatCurrency } from '../utilities/formatCurrency'
+import placeholderProduct from '../assets/placeholder.jpg'
 
 interface Props {
     product: Product
@@ -11,7 +12,7 @@ const ProductCard = ({product}: Props) => {
   return (
     <>
         <Card borderRadius={10} overflow="hidden">
-            <Image height={300} objectFit={"fill"} src={product.img} fallbackSrc='https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg'></Image>
+            <Image height={300} objectFit={"fill"} src={product.img} fallbackSrc={placeholderProduct}></Image>
             <CardBody>
                 <Heading>{product.name}</Heading>
                 <Text>{formatCurrency(product.price)}</Text>
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,13 +1,15 @@
 // LoginPage.js
 import React, { useState } from 'react';
-import { Button, Input, Alert, AlertIcon } from '@chakra-ui/react';
+import { Button, Input, Alert, AlertIcon, Grid, Stack, Heading } from '@chakra-ui/react';
 import { useAuth } from '../contexts/AuthContext';
+import { useNavigate } from 'react-router-dom';
 
 const LoginPage: React.FC = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
   const { login } = useAuth();
+  const navigate = useNavigate();
 
   const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -23,6 +25,7 @@ const LoginPage: React.FC = () => {
       if (response.ok) {
         const data = await response.json();
         login(data.token, data.role); // Store the token and role
+        navigate('/');
       } else {
         setError('Invalid username or password.');
       }
@@ -32,29 +35,36 @@ const LoginPage: React.FC = () => {
   };
 
   return (
-    <div>
-      {error && (
-        <Alert status="error">
-          <AlertIcon />
-          {error}
-        </Alert>
-      )}
-      <form onSubmit={handleLogin}>
-        <Input
-          type="text"
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <Input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <Button colorScheme="blue" type="submit">Login</Button>
-      </form>
-    </div>
+    <Grid justifyContent={'center'}>
+      <Stack paddingTop={"100px"} alignContent={'center'} maxWidth={"300px"}>
+        <Heading as={"u"}>Login</Heading>
+        {error && (
+          <Alert status="error">
+            <AlertIcon />
+            {error}
+          </Alert>
+        )}
+        <form onSubmit={handleLogin}>
+          <Input
+            marginTop={"20px"}
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <Input
+          marginTop={"20px"}
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Stack marginTop={"20px"}  align={'center'}>
+            <Button width={"150px"} colorScheme="blue" type="submit">Login</Button>
+          </Stack>
+        </form>
+      </Stack>
+    </Grid>
   );
 };
 
